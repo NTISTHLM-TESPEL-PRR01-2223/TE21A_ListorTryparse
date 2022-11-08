4a2ce1b6075b0d3392c7d21c1d3431617e6286b5
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-11-08 11:06:46 +0100

    Lade in tryparse & listor

--- TE21A_ListorTryparse/Program.cs
+++ TE21A_ListorTryparse/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<string> teachers = new();
+
+teachers.Add("Micke");
+teachers.Add("Martin");
+teachers.Add("Karim");
+
+Random generator = new();
+int i = generator.Next(teachers.Count);
+
+Console.WriteLine(teachers[i]);
+
+
+Console.ReadLine();
+
+
+// int money = 0;
+// bool success = false;
+
+// while (success == false)
+// {
+//   Console.WriteLine("Hur mycket pengar vill du satsa? Skriv en siffra.");
+//   string moneyText = Console.ReadLine();
+//   success = int.TryParse(moneyText, out money);
+
+//   if (success == false)
+//   {
+//     Console.WriteLine("En siffra sa jag, sopa!");
+//   }
+// }
+
+// Console.WriteLine("yay");
+
+
